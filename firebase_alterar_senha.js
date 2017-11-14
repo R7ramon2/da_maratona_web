d9d5f4b35e5c52b65ff622766ad06021f10f77e9
Tiagoemerenciano <emerenciano.tiago@gmail.com>
2017-11-14 14:19:56 -0300

    Alteração de senha melhorada.

--- firebase_alterar_senha.js
+++ firebase_alterar_senha.js
@@ -1,19 +1,8 @@
 $(document).ready(function () {
     $("#matricula").mask("000000000-0");
-    $("#senha").mask("000000");
-    $("#senha_confirmacao").mask("000000");
 
     $("#inserir").click(function () {
         matricula = $("#matricula").val();
-        senha = $("#senha").val();
-        senhaConfirmacao = $("#senha_confirmacao").val();
-        senhaCript = senha.split("");
-        senhaCript[0] *= 24 * 3 * 2 * 2;
-        senhaCript[1] *= 13 * 7 * 2 * 7;
-        senhaCript[2] *= 69 * 5 * 8 * 11;
-        senhaCript[3] *= 21 * 7 * 5 * 9;
-        senhaCript[4] *= 19 * 12 * 7 * 3;
-        senhaCript[5] *= 97 * 9 * 2;
 
         if (matricula === "") {
             alert("Matrícula obrigatória!");
@@ -23,39 +12,14 @@ $(document).ready(function () {
             alert("Matrícula incompleta!");
             document.getElementById("matricula").focus();
         }
-        else if (senha === "") {
-            alert("Senha obrigatória!");
-            document.getElementById("senha").focus();
-        }
-        else if (senha.length < 6) {
-            alert("Senha incompleta!");
-            document.getElementById("senha").focus();
-        }
-        else if (senhaConfirmacao === "") {
-            alert("Senha incompleta!");
-            document.getElementById("senha").focus();
-        }
-        else if (senhaConfirmacao.length < 6) {
-            alert("Senha incompleta!");
-            document.getElementById("senha").focus();
-        }
-        else if (senha !== senhaConfirmacao){
-            alert("Senhas diferentes!");
-            document.getElementById('senha').value = '';
-            document.getElementById('senha_confirmacao').value = '';
-            document.getElementById("senha").focus();
-        }
         else {
-            senha = senhaCript.join("");
             const dbRefMat = firebase.database().ref().child('Alunos/' + matricula + '/matricula');
             dbRefMat.once('value', snap => {
                 const dados = snap.val();
             if (dados !== null) {
-                firebase.database().ref().child('Alunos/' + matricula + '/senha').set(senha);
-                alert("Senha alterada com sucesso!");
+                firebase.database().ref().child('Alunos/' + matricula + '/verificado').set(false);
+                alert("Aluno poderá alterar sua senha no próximo login!");
                 document.getElementById('matricula').value = '';
-                document.getElementById('senha').value = '';
-                document.getElementById('senha_confirmacao').value = '';
             }
             else {
                 alert("Matrícula inválida!");
